12b91de77e998f40c6ceea04ef5aece04164b7c5
MEHROJ <mekhrojmajidov@gmail.com>
2023-10-26 17:50:26 +0500

    Added full conversion compability
    Now converts from decimal to any others too

--- main.cpp
+++ main.cpp
@@ -19,6 +19,20 @@ string fix_hex(string item_to_fix) {
     return result;
 }
 
+string break_hex(string item_to_fix) {
+    string result = item_to_fix;
+    string hexadecimal_letters[6] = {"A","B","C","D","E","F"};
+    string hexadecimal_integers[6] = {"10","11","12","13","14","15"};
+
+    for (int i=0; i < 7; i++){
+        if (hexadecimal_integers[i] == item_to_fix){
+            result = hexadecimal_letters[i];
+            break;
+        };
+    };
+    
+    return result;
+}
 
 string any_to_decimal(int base, string number){
     string result, num;
@@ -39,9 +53,37 @@ string any_to_decimal(int base, string number){
     return result;
 }
  
+string decimal_to_any(int base, long long int number){
+    /*Converts from decimal to binary, octal, hexadecimal numeric systems*/
+    string result, cache;
+    long long int whole_part=base, temp=number; 
+    int remainder_part, count=0;
+
+    while (whole_part >= base){ //
+        whole_part = temp/base;
+        remainder_part = temp%base;
+        if (remainder_part >= 10 && base == 16){
+            cache = break_hex(to_string(remainder_part));
+            result.insert(0, cache);
+            temp = temp/base;
+            count++;
+            continue;
+        };
+        result.insert(0, to_string(remainder_part));
+        temp = temp/base;
+        count++;
+    };
+
+    result.insert(0, to_string(whole_part));
+
+    return result;
+}
+
+
 int main(){
 
     cout << any_to_decimal(8, "46547213657") << endl;
- 
+    cout << decimal_to_any(16, 73027) << endl;
+
     return 0;
 }
