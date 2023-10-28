8a4ed6119ea8662a8da41396300b4ace2084c284
MEHROJ <mekhrojmajidov@gmail.com>
2023-10-28 08:14:07 +0500

    Preparing for frontend part
    Declared requirements and tested example window

---
+++ main.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+root = ctk.CTk()
+root.title("Numeric System Conversion")
+root.geometry("300x200")
+
+label = ctk.CTkLabel(root, text="Hello World!")
+label.pack()
+
+root.mainloop()
